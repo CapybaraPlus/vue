22839c1f0cbb6c703f2c064328959f07591b76a7
ranan <ranan@email.com>
2024-05-09 20:42:28 +0800

    修改了一些描述

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import CapybaraUI from "@capybara-ui/vue"
+import CapybaraUI from "@capybara-ui/components"
 
 createApp(App).use(CapybaraUI).mount('#app')
