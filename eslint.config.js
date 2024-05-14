030ca47326cf098fa78bb0c118b8fd302486d868
ranan <ranan@email.com>
2024-05-14 11:08:06 +0800

    eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { 
+    languageOptions: 
+    { 
+      globals: { 
+        ...globals.browser, 
+        ...globals.node 
+      } 
+    } 
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
+  ...pluginVue.configs["flat/recommended"],
 ];
--- packages/components/index.ts
+++ packages/components/index.ts
@@ -5,3 +5,4 @@ export default {
     install(app: App) {
         app.component("RaButton", Button)
     }
+}
