e80833e024c9f8db131e452e0d01c7d2de9bb44c
ranan <ranan@email.com>
2024-05-16 10:54:17 +0800

    test: 测试commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    /**
-     * type(scope): <subject>
-     */
-    'type-enum': [
-      2, // 错误级别，0为关闭，1为警告，2为错误
-      'always', // 是否应用，always/never
-      [
-        'feat', // 新功能（feature）
-        'fix', // 修复bug
-        'docs', // 修改文档
-        'style', // 修改代码格式，不影响代码逻辑
-        'refactor', // 代码重构，理论上不影响功能逻辑
-        'test', // 修改测试用例
-        'build', // 构建或其他工具的变动(如webpack)
-        'revert', // 还原以前的提交
-        'merge', // 分支代码合并
-        'chore', // 构建过程或辅助工具的变动
-      ],
-    ],
     'scope-enum': [
       2,
       'always',
@@ -38,6 +19,25 @@ export default {
         'lint', // 代码检查
         'other', // 其它
       ]
+    ],
+    /**
+     * type(scope): <subject>
+     */
+    'type-enum': [
+      2, // 错误级别，0为关闭，1为警告，2为错误
+      'always', // 是否应用，always/never
+      [
+        'feat', // 新功能（feature）
+        'fix', // 修复bug
+        'docs', // 修改文档
+        'style', // 修改代码格式，不影响代码逻辑
+        'refactor', // 代码重构，理论上不影响功能逻辑
+        'test', // 修改测试用例
+        'build', // 构建或其他工具的变动(如webpack)
+        'revert', // 还原以前的提交
+        'merge', // 分支代码合并
+        'chore', // 构建过程或辅助工具的变动
+      ],
     ]
   },
 }
