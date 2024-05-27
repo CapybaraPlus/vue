d12df4cf55a344882a1184a63d42bd487196f08d
ranan <ranan@email.com>
2024-05-27 13:48:12 +0800

    docs: 修改代码文档描述

--- packages/hooks/use-classname/index.ts
+++ packages/hooks/use-classname/index.ts
@@ -1,53 +1,56 @@
-const defaultNameSpace = "ra"; // 默认的类名前缀
+const defaultNameSpace = 'ra' // default namespace
 
 const generateClassName = (
-  block: string, // 块
-  blockSuffix?: string, // 块后缀
-  element?: string, // 块的子元素
-  modifier?: string // 块的修饰符
+  block: string,
+  blockSuffix?: string,
+  element?: string,
+  modifier?: string
 ) => {
-  let className = `${defaultNameSpace}-${block}`;
+  let className = `${defaultNameSpace}-${block}`
   if (blockSuffix) {
-    className += `-${blockSuffix}`;
+    className += `-${blockSuffix}`
   }
   if (element) {
-    className += `__${element}`;
+    className += `__${element}`
   }
   if (modifier) {
-    className += `--${modifier}`;
+    className += `--${modifier}`
   }
-  return className;
-};
+  return className
+}
 
-// 当传入button时
+// when block is button
 export const useClassName = (block: string) => {
-  // 生成block或者blockSuffix类名，例如：ra-button、ra-button-group
-  const b = (blockSuffix?: string) => 
-    generateClassName(block, blockSuffix, "", "");
+  // generate block or block-suffix class name such as ra-button、ra-button-group
+  const b = (blockSuffix?: string) =>
+    generateClassName(block, blockSuffix, '', '')
 
-  // 生成element类名，例如：ra-button__icon、ra-button__text
-  const e = (element?: string) => 
-    generateClassName(block, "", element, "");
+  // generate element class name such as ra-button__icon、ra-button__text
+  const e = (element?: string) => generateClassName(block, '', element, '')
 
-  // 生成modifier类名，例如：ra-button--primary、ra-button--disabled
-  const m = (modifier?: string) => 
-    generateClassName(block, "", "", modifier);
+  // generate modifier class name such as ra-button--primary、ra-button--disabled
+  const m = (modifier?: string) => generateClassName(block, '', '', modifier)
 
-  // 生成blockSuffix+element类名，例如：ra-button-group__wrapper
+  // generate block-suffix and element class name such as ra-button-group__wrapper
   const be = (blockSuffix?: string, element?: string) =>
-    generateClassName(block, blockSuffix, element, "");
+    generateClassName(block, blockSuffix, element, '')
 
-  // 生成blockSuffix+modifier类名，例如：ra-button-group--primary
+  // generate block-suffix and modifierclass name such as ra-button-group--primary
   const bm = (blockSuffix?: string, modifier?: string) =>
-    generateClassName(block, blockSuffix, "", modifier);
+    generateClassName(block, blockSuffix, '', modifier)
 
-  // 生成bem类名，例如：ra-button__icon--left
+  // generate bem class name such as ra-button__icon--left
   const em = (element?: string, modifier?: string) =>
-    generateClassName(block, "", element, modifier);
+    generateClassName(block, '', element, modifier)
 
-  // 生成blockSuffix+em类名，例如：ra-button-group__icon--left
+  // generate block-suffix and em class name such as ra-button-group__icon--left
   const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
-    generateClassName(block, blockSuffix, element, modifier);
+    generateClassName(block, blockSuffix, element, modifier)
+
+  // generate state class name
+  const is = (state: string) => {
+    return `is-${state}`
+  }
 
   return {
     b,
@@ -57,5 +60,6 @@ export const useClassName = (block: string) => {
     bm,
     em,
     bem,
-  };
-};
+    is,
+  }
+}
