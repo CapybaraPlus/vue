8722866e27ac2929103238da1b914e70ceb30223
ranan <ranan@email.com>
2024-06-02 19:57:18 +0800

    style: button组件主题样式

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <ra-button>按钮</ra-button>
+  <ra-button theme="default">default button</ra-button>
+  <ra-button theme="primary">primary button</ra-button>
+  <ra-button theme="success">success button</ra-button>
+  <ra-button theme="warning">warning button</ra-button>
+  <ra-button theme="danger">danger button</ra-button>
 </template>
 
 <script setup lang="ts"></script>
