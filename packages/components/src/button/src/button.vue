166c088fbb07897e86cba9d9b975c80e28c0407b
ranan <ranan@email.com>
2024-06-02 19:33:13 +0800

    fix: 修复useClassName的函数中如果传入空参同样也会生成类名导致类名重复的问题

--- packages/components/src/button/src/button.vue
+++ packages/components/src/button/src/button.vue
@@ -1,10 +1,11 @@
 <template>
-  <button :class="[ucn.b()]" @click="handleClick">
+  <button :class="[ucn.b(), ucn.m(_theme)]" @click="handleClick">
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useClassName, useComponetName } from '@capybara-ui/hooks'
 import { buttonProps, buttonEmits } from './button'
 import '../styles/index'
@@ -14,12 +15,16 @@ defineOptions({
   name: useComponetName('button'),
 })
 
-defineProps(buttonProps)
+const $props = defineProps(buttonProps)
 const $emit = defineEmits(buttonEmits)
 
 const handleClick = (e: MouseEvent) => {
   $emit('click', e)
 }
+
+const _theme = computed(() =>
+  $props.theme == 'default' ? undefined : $props.theme
+)
 </script>
 
 <style scoped></style>
--- packages/hooks/use-classname/index.ts
+++ packages/hooks/use-classname/index.ts
@@ -26,30 +26,38 @@ export const useClassName = (block: string) => {
     generateClassName(block, blockSuffix, '', '')
 
   // generate element class name such as ra-button__icon、ra-button__text
-  const e = (element?: string) => generateClassName(block, '', element, '')
+  const e = (element?: string) =>
+    element ? generateClassName(block, '', element, '') : ''
 
   // generate modifier class name such as ra-button--primary、ra-button--disabled
-  const m = (modifier?: string) => generateClassName(block, '', '', modifier)
+  const m = (modifier?: string) =>
+    modifier ? generateClassName(block, '', '', modifier) : ''
 
   // generate block-suffix and element class name such as ra-button-group__wrapper
   const be = (blockSuffix?: string, element?: string) =>
-    generateClassName(block, blockSuffix, element, '')
+    blockSuffix && element
+      ? generateClassName(block, blockSuffix, element, '')
+      : ''
 
   // generate block-suffix and modifierclass name such as ra-button-group--primary
   const bm = (blockSuffix?: string, modifier?: string) =>
-    generateClassName(block, blockSuffix, '', modifier)
+    blockSuffix && modifier
+      ? generateClassName(block, blockSuffix, '', modifier)
+      : ''
 
   // generate bem class name such as ra-button__icon--left
   const em = (element?: string, modifier?: string) =>
-    generateClassName(block, '', element, modifier)
+    element && modifier ? generateClassName(block, '', element, modifier) : ''
 
   // generate block-suffix and em class name such as ra-button-group__icon--left
   const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
-    generateClassName(block, blockSuffix, element, modifier)
+    blockSuffix && element && modifier
+      ? generateClassName(block, blockSuffix, element, modifier)
+      : ''
 
   // generate state class name
   const is = (state: string) => {
-    return `is-${state}`
+    return state ? `is-${state}` : ''
   }
 
   return {
