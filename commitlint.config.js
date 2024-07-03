59869ad18370d52f193f7d1f0e700f73dec6b6e2
ranan <ranan@email.com>
2024-07-03 09:56:35 +0800

    chore(other): 修改commitlint的提交信息验证模式

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,42 +2,40 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [
-      2,
-      'always',
+      2, // the level of the rule: 0 = disable, 1 = warning, 2 = error
+      'always', // whether applicate ,always/never
       [
-        'deps', // 依赖
-        'config', // 配置
-        'utils', // 工具
-        'components', // 组件
-        'styles', // 样式
-        'hooks', // 钩子
-        'plugins', // 插件
-        'test', // 测试
-        'release', // 发布
-        'ci', // 持续集成
-        'docs', // 文档
-        'lint', // 代码检查
-        'other', // 其它
-      ]
+        'deps', // dependencies
+        'config', // configuration
+        'utils', // package utils
+        'components', // package components
+        'styles', // package theme-chalk
+        'hooks', // package hooks
+        'test', // test
+        'release', // release
+        'lint', // code lint
+        'play', // playground
+        'other', // other
+      ],
     ],
     /**
      * type(scope): <subject>
      */
     'type-enum': [
-      2, // 错误级别，0为关闭，1为警告，2为错误
-      'always', // 是否应用，always/never
+      2,
+      'always',
       [
-        'feat', // 新功能（feature）
-        'fix', // 修复bug
-        'docs', // 修改文档
-        'style', // 修改代码格式，不影响代码逻辑
-        'refactor', // 代码重构，理论上不影响功能逻辑
-        'test', // 修改测试用例
-        'build', // 构建或其他工具的变动(如webpack)
-        'revert', // 还原以前的提交
-        'merge', // 分支代码合并
-        'chore', // 构建过程或辅助工具的变动
+        'feat', // A new feature
+        'fix', // A bug fix
+        'docs', // Documentation only changes
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'perf', // A code change that improves performance
+        'test', // Adding missing tests or correcting existing tests
+        'build', // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+        'chore', // Other changes that don't modify src or test files
+        'revert', // Reverts a previous commit
       ],
-    ]
+    ],
   },
 }
