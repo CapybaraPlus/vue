f43fa0772705446e8a3636634b4fc2fd2c3742cc
ranan <ranan@email.com>
2024-07-04 15:11:11 +0800

    feat(components): 导出input示例

--- packages/components/src/input/index.ts
+++ packages/components/src/input/index.ts
@@ -1,7 +1,10 @@
-import { withInstall } from '@capybara-plus/utils'
+import { withInstall, buildInstance } from '@capybara-plus/utils'
 import Input from './src/input.tsx'
 
 export const RaInput = withInstall(Input)
 export default RaInput
 
 export * from './src/input.ts'
+
+// typescript instance of the component
+export type RaInputInstance = buildInstance<typeof RaInput>
